368a1ba26ac96c284ef6cd210e1a39000f9153df
luishi1 <luis.oviedo.et26@gmail.com>
2023-05-20 19:12:27 -0300

    SE TERMINO LA TORTURA DIOS ES DIOS

--- flappy/proyecto/Assets/Script/Bird.cs
+++ flappy/proyecto/Assets/Script/Bird.cs
@@ -13,9 +13,14 @@ public class Bird : MonoBehaviour
     public CharacterDatabase characterDB;
     public SpriteRenderer artworkSprite;
     private int selectedOption = 0;
+    //animation
+    public Animator animator;
+    public RuntimeAnimatorController nuevoControlador0;
+    public RuntimeAnimatorController nuevoControlador1;
+    public RuntimeAnimatorController nuevoControlador2;
 
-
-    private void Awake(){
+    private void Awake()
+    {
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         rotateBird = GetComponent<RotateBird>();
@@ -24,15 +29,17 @@ public class Bird : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if(isDead) return;
-        if(Input.GetMouseButtonDown(0) && transform.position.y < 5){
+        if (isDead) return;
+        if (Input.GetMouseButtonDown(0) && transform.position.y < 5)
+        {
             rb2d.velocity = Vector2.zero;
             rb2d.AddForce(Vector2.up * upForce);
             anim.SetTrigger("Flap");
             SoundSystem.instance.PlayFlap();
         }
     }
-    private void OnCollisionEnter2D (Collision2D collision){
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         isDead = true;
         anim.SetTrigger("Die");
         rotateBird.enabled = false;
@@ -45,25 +52,27 @@ public class Bird : MonoBehaviour
     //skin
     private void UpdateCharacter(int selectedOption)
     {
-    Character character = characterDB.GetCharacter(selectedOption);
-    artworkSprite.sprite = character.characterSprite;
-    }
-
-    private void load()
-    {   
-    selectedOption = PlayerPrefs.GetInt("selectedOption");
+        Character character = characterDB.GetCharacter(selectedOption);
+        artworkSprite.sprite = character.characterSprite;
     }
 
     void Start()
     {
-    if(PlayerPrefs.HasKey("selectedOption"))
-    {
-        selectedOption = 0;
-    }
-    else
-    {
-        load();
-    }
-    UpdateCharacter(selectedOption);
+        if (PlayerPrefs.HasKey("selectedOption"))
+        {
+            selectedOption = PlayerPrefs.GetInt("selectedOption");
+        }
+        if (selectedOption == 1)
+        {
+                  anim.runtimeAnimatorController = nuevoControlador1;
+        }
+        else if (selectedOption == 2)
+        {
+             anim.runtimeAnimatorController = nuevoControlador2;
+        }
+        else
+        {
+           anim.runtimeAnimatorController = nuevoControlador0;
+        }
     }
 }
