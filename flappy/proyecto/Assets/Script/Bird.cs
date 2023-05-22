a0f2c38f4c598b4cd6295132dffc288fef988694
MamushkaDead <gasparkappou@gmail.com>
2023-05-22 18:18:36 -0300

    cambios importantes, no cambiar

--- flappy/proyecto/Assets/Script/Bird.cs
+++ flappy/proyecto/Assets/Script/Bird.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     private bool isDead = false, listo = false;
-    private Rigidbody2D rb2d;
+    public Rigidbody2D rb2d;
     private Animator anim;
     public float upForce = 200f;
     private RotateBird rotateBird;
@@ -39,14 +39,17 @@ public class Bird : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        listo = true;
-        if (isDead) return;
-        if (Input.GetMouseButtonDown(0) && transform.position.y < 5)
+        if (Input.GetMouseButtonDown(0) || listo == true)
         {
-            rb2d.velocity = Vector2.zero;
-            rb2d.AddForce(Vector2.up * upForce);
-            anim.SetTrigger("Flap");
-            SoundSystem.instance.PlayFlap();
+            listo = true;
+            if (isDead) return;
+            if (Input.GetMouseButtonDown(0) && transform.position.y < 5)
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(Vector2.up * upForce);
+                anim.SetTrigger("Flap");
+                SoundSystem.instance.PlayFlap();
+            }
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
