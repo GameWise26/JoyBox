50f13d8d9de898bee65d5fccbbc979bf06125908
angieVentura <angiev.d.23@gmail.com>
2023-07-20 23:20:12 -0300

    Seguridad de la contraseña

--- interfaz/Assets/Scripts/Register.cs
+++ interfaz/Assets/Scripts/Register.cs
@@ -8,64 +8,86 @@ using UnityEngine.SceneManagement;
 using Newtonsoft.Json;
 using System.Text.RegularExpressions;
 using System.Globalization;
+using System.Linq;
+using System.Text;
 
-class Formulario{
+class Formulario
+{
     public string nombre { get; set; }
-    public string edad{get;set;}
-    public string contrasenia{get;set;}
-    public string rcontrasenia{get;set;}
-    public string correo{get;set;}
+    public string edad { get; set; }
+    public string contrasenia { get; set; }
+    public string rcontrasenia { get; set; }
+    public string correo { get; set; }
 }
 
 public class Register : MonoBehaviour
 {
-    public TextMeshProUGUI usuario,contrasenia,email,rcontrasenia,edad,msgbox;
+    public TextMeshProUGUI usuario, contrasenia, email, rcontrasenia, edad, msgbox;
 
     // Start is called before the first frame update
     void Start()
     {
         SocketManager.instancia.socket.OnUnityThread("registro", (response) =>
         {
-            Dictionary<string,bool> res = JsonConvert.DeserializeObject<Dictionary<string,bool>>(response.ToString().Split('[')[1].Split(']')[0]);
-            if(res["exito"]) msgbox.text = "Se registro correctamente, ahora inicie sesion";
+            Dictionary<string, bool> res = JsonConvert.DeserializeObject<Dictionary<string, bool>>(response.ToString().Split('[')[1].Split(']')[0]);
+            if (res["exito"]) msgbox.text = "Se registro correctamente, ahora inicie sesion";
             else msgbox.text = "No se pudo registrar, verifique los datos ingresados";
         });
     }
 
-    public void Enviar(){
+    public void Enviar()
+    {
 
         if (!ValidarDatos())
-            return; 
+            return;
 
-        Formulario form = new Formulario{
+        Formulario form = new Formulario
+        {
             nombre = usuario.text,
             edad = edad.text,
             contrasenia = contrasenia.text,
             rcontrasenia = rcontrasenia.text,
             correo = email.text
         };
-        SocketManager.instancia.socket.Emit("registro",JsonConvert.SerializeObject(form));
+        SocketManager.instancia.socket.Emit("registro", JsonConvert.SerializeObject(form));
     }
 
     private bool ValidarDatos()
     {
-        if (string.IsNullOrEmpty(usuario.text) || string.IsNullOrEmpty(edad.text)|| string.IsNullOrEmpty(contrasenia.text) || string.IsNullOrEmpty(rcontrasenia.text) || string.IsNullOrEmpty(email.text))
+        if (string.IsNullOrEmpty(usuario.text) || string.IsNullOrEmpty(edad.text) || string.IsNullOrEmpty(contrasenia.text) || string.IsNullOrEmpty(rcontrasenia.text) || string.IsNullOrEmpty(email.text))
         {
             msgbox.text = "Todos los campos son obligatorios";
+            return false;
+        }
+
+        if (!int.TryParse(edad.text, out int edadNum)) 
+        { 
+            msgbox.text = "Fallo la conversion"; 
+            return false; 
+        }
+
+        if ( edadNum > 99 || edadNum < 5)
+        {
+            msgbox.text = "Ingrese una edad válida";
+            return false;
+        }
+
+
+        if (ValidarFormatoCorreo(email.text) == false)
+        {
+            msgbox.text = "El formato del correo electrónico es inválido";
+            return false;
+        }
+
+        if (ValidarSeguridadContrasenia(contrasenia.text).Any(c => c != null))
+        {
+            msgbox.text = $"La contraseña al menos debe tener:\n{string.Join("\n", ValidarSeguridadContrasenia(contrasenia.text).Where(c => c != null))}";
             return false;
         }
 
         if (contrasenia.text != rcontrasenia.text)
         {
             msgbox.text = "Las contraseñas no coinciden";
-            return false;
-        }
-
-        Debug.Log(email.text);
-      
-        if (ValidarFormatoCorreo(email.text) == false)
-        {
-            msgbox.text = "El formato del correo electrónico es inválido";
             return false;
         }
 
@@ -92,11 +114,11 @@ public class Register : MonoBehaviour
                 return match.Groups[1].Value + domainName;
             }
         }
-        catch (RegexMatchTimeoutException e)
+        catch (RegexMatchTimeoutException )
         {
             return false;
         }
-        catch (ArgumentException e)
+        catch (ArgumentException )
         {
             return false;
         }
@@ -112,8 +134,25 @@ public class Register : MonoBehaviour
 
     }
 
+    private string[] ValidarSeguridadContrasenia(string contrasenia)
+    {
+        contrasenia = contrasenia.Trim();
 
-    public void irAlLogin(){
+        char[] CaracteresEspaciales = { '!', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~' };
+
+        string[] sugerencias = {
+        Regex.IsMatch(contrasenia, @"[A-Z]") ? null : "Una mayúscula",
+        Regex.IsMatch(contrasenia, @"[a-z]") ? null : "Una minúscula",
+        Regex.IsMatch(contrasenia, @"\d") ? null : "Un número",
+        contrasenia.Any(c => CaracteresEspaciales.Contains(c)) ? null : "Un caracter especial",
+        contrasenia.Length >= 9 && contrasenia.Length <= 20 ? null : "Entre 9 y 20 caracteres"
+    };
+        return sugerencias;
+    }
+
+
+    public void irAlLogin()
+    {
         SceneManager.LoadScene("interfaz_inicio_sesion");
     }
 }
